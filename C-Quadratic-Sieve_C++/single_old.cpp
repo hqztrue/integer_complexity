ac1cf8554be34eed7e705293446004392556463d
hqztrue <hqztrue@sina.com>
2023-08-31 23:26:41 +0800

    improve algo
    using a better prune strategy.

--- C-Quadratic-Sieve_C++/single_old.cpp
+++ C-Quadratic-Sieve_C++/single_old.cpp
@@ -31,19 +31,15 @@ void println(u128 x){print(x); putchar('\n');}
 u128 u128_from_str(const char *s){u128 x=0; while (*s)x=x*10+*s++-'0'; return x;}
 inline u128 _rand128(){static u128 x=u128_from_str("199609092119960909211996090921996090921");x+=(x<<17)+(x>>29)+1;return x;}
 
-u128 calc_g(int n){  // compute the maximum integer with complexity n
+u128 calc_g(int n){
 	u128 res=1;
 	while (n>=5||n==3)res*=3,n-=3;
 	return res<<n/2;
 }
-int defect_approx(u128 n,int cur){
+int defect(u128 n,int cur){
 	const double eps=1e-3;
 	for (int i=1;;++i)
 		if (calc_g(i)*(1+eps)>=n)return max(cur-i,0);
-}
-int complexity_LB(u128 n){  //todo: tighter (+1)
-	for (int i=1;;++i)
-		if (calc_g(i)>=n)return i;
 }
 void calc_all(uint n){  //computes f(i) for all i<=n.
 	a=(uchar*)malloc(n+1); a[1]=1;
@@ -123,50 +119,28 @@ vector<T> get_factors(T x){
 	if (sizeof(T)==sizeof(ull))return get_factors_from_primes(prime_factors(x));
 	else return get_factors_from_primes(prime_factors128(x));
 }
-ushort dfs(ull x,int t){  //decides whether f(x)<=t. If true, return the optimal f(x).
+ushort dfs(ull x,int t=10){  //computes f(x) for a single x, overall using <=t 1's for addition.
 	if (x<=n0)return a[x];
 	auto it=H.find(x);
 	if (it!=H.end()&&it->second.first>=t)return it->second.second;
 	ushort ans=inf1;
-	for (int i=1;i<x;++i){
-		const int C=1000;
-		if (x-i>C&&complexity_LB(i)+complexity_LB(x-i-C)>t)break;  //to check
-		ushort lb=complexity_LB(x-i);
-		if (a[i]+lb<=t)ans=min(ans,ushort(dfs(x-i,t-a[i])+a[i]));
-	}
+	for (int i=1;i<=g[t]&&i<x;++i)
+		if (a[i]<=t)ans=min(ans,ushort(dfs(x-i,t-a[i])+a[i]));
 	for (auto &g:get_factors(x))
-		if (g>1&&g<=x/g){
-			ushort lb1=complexity_LB(g),lb2=complexity_LB(x/g);
-			if (lb1+lb2>t)continue;
-			ushort v1=dfs(g,t-lb2);
-			if (v1+lb2>t)continue;
-			ushort v2=dfs(x/g,t-v1);
-			ans=min(ans,ushort(v1+v2));
-		}
+		if (g>1&&g<=x/g)ans=min(ans,ushort(dfs(g,t)+dfs(x/g,t)));
 	H[x]=make_pair(t,ans);
 	return ans;
 }
-ushort dfs128(u128 x,int t){
+ushort dfs128(u128 x,int t=10){
 	if (x<=n0)return a[x];
 	if (x<U)return dfs(x,t);
 	auto it=H1.find(x);
 	if (it!=H1.end()&&it->second.first>=t)return it->second.second;
 	ushort ans=inf1;
-	for (int i=1;i<x;++i){
-		const int C=1000;
-		if (x-i>C&&complexity_LB(i)+complexity_LB(x-i-C)>t)break;  //to check
-		ushort lb=complexity_LB(x-i);
-		if (a[i]+lb<=t)ans=min(ans,ushort(dfs128(x-i,t-a[i])+a[i]));
-	}
+	for (int i=1;i<=g[t]&&i<x;++i)
+		if (a[i]<=t)ans=min(ans,ushort(dfs128(x-i,t-a[i])+a[i]));
 	for (auto &g:get_factors(x))
-		if (g>1&&g<=x/g){
-			ushort lb1=complexity_LB(g),lb2=complexity_LB(x/g);
-			if (lb1+lb2>t)continue;
-			ushort v1=dfs128(g,t-lb2);
-			if (v1+lb2>t)continue;
-			ushort v2=dfs128(x/g,t-v1);
-			ans=min(ans,ushort(v1+v2));
-		}
+		if (g>1&&g<=x/g)ans=min(ans,ushort(dfs128(g,t)+dfs128(x/g,t)));
 	H1[x]=make_pair(t,ans);
 	return ans;
 }
@@ -182,12 +156,11 @@ void clear(){
 	//unordered_map<ull,pair<uchar,ushort>>().swap(H);
 	//unordered_map<u128,pair<uchar,ushort>>().swap(H1);
 }
-void check1(){  // test the conjecture f(p^i)=i*f(p). (In particular, f(2^i)=2i.)
+void check1(){  // test the conjecture f(p^i)=i*f(p).
 	int t1=clock();
-	//vector<int> primes={733,379,739,541};  // conjecture fails
-	//vector<int> primes={577,811,109};
+	vector<int> primes={577,811,109};
 	//vector<int> primes={433,163,487,2};
-	vector<int> primes={2};
+	//vector<int> primes={2};
 	for (auto mul:primes){
 		printf("mul=%I64d\n",mul);
 		u128 x=mul;
@@ -195,17 +168,9 @@ void check1(){  // test the conjecture f(p^i)=i*f(p). (In particular, f(2^i)=2i.
 			//if (i<10)continue;
 			n=x;
 			//printf("i=%d n=%I64d\n",i,n);
-			int v=i*a[mul],lb=complexity_LB(n);  // v: the conjectured complexity. d: defect.
-			//printf("def=%d\n",d);
-			int ans=inf1;
-			for (int t=lb;t<v;++t){
-				int res=dfs128(n,t);
-				ans=min(ans,res);
-				if (ans<v){
-					printf("improve: %d %d\n",ans,v);
-					break;
-				}
-			}
+			int v=i*a[mul],d=defect(n,v-1);  // v: the conjectured complexity. d: defect.
+			printf("def=%d\n",d);
+			int ans=dfs128(n,d);
 			printf("i=%d ans=%d\n",i,ans);
 			//printf("time=%d\n",clock()-t1);
 			//printf("size=%d\n",H.size());
@@ -238,7 +203,7 @@ void check2(){  // test the conjecture f(2^i3^j5^k)=2i+3j+5k (k<=5)
 				if (i<60||i==60&&j<3||i==60&&j==3&&k<5)continue;
 				//if (i<58)continue;
 				printf("i=%d j=%d k=%d ",i,j,k); println(n);
-				int v=i*2+j*3+k*5,d=defect_approx(n,v-1);
+				int v=i*2+j*3+k*5,d=defect(n,v-1);
 				int ans=dfs128(n,d);
 				if (ans<v){
 					printf("err: %d %d",ans,v); println(n);
@@ -260,7 +225,7 @@ void check3(){  // test the conjecture f(2^i+1)=2i+1.
 			if (i<=76)continue;
 			n=x+1;
 			//printf("i=%d n=%I64d\n",i,n);
-			int v=i*2+1,d=defect_approx(n,v-1);
+			int v=i*2+1,d=defect(n,v-1);
 			printf("def=%d\n",d);
 			int ans=dfs128(n,d);
 			printf("i=%d ans=%d\n",i,ans);
@@ -316,7 +281,7 @@ void run_sample(int num_samples=1e4,bool debug=1){
 		n=_rand128()%N0+2;
 		//int d=6;
 		ushort ans=10000,ans1=ans;
-		for (int d=0;d<=1000&&d<=defect_approx(n,ans-1);++d){
+		for (int d=0;d<=6&&d<=defect(n,ans-1);++d){  //100
 			//printf("def=%d\n",d);
 			ushort cur=dfs128(n,d);
 			ans=min(ans,cur);
@@ -360,17 +325,16 @@ int main()
 	//u128 N0=1; //N0<<=120;
 	//for (int i=1;i<=23;++i)N0*=10;
 	//init(1e7,1e16);
-	//init(1e8,1e20);
-	init(2e9,1e35);
+	init(1e9);
 	//init(1e9,1e20);
 	//init(2e9,1e25);
 	
-	check1();
+	//check1();
 	//check2();
 	//check3();
 	//check4();
 	//run_sample(1e4);
-	/*while (1){
+	while (1){
 		N0=1e19;
 		run_sample(500,0);
 		return 0;
@@ -380,7 +344,7 @@ int main()
 		N0=i;
 		//run_sample(1e6);
 		run_sample(1e5);
-	}*/
+	}
     return 0;
 }
 
