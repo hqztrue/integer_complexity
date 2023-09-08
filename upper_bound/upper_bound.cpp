1daa9ddf267b3b5013223098e686d8b4f0ca0006
hqztrue <hqztrue@sina.com>
2023-09-08 09:15:40 +0800

    .

--- upper_bound/upper_bound.cpp
+++ upper_bound/upper_bound.cpp
@@ -39,15 +39,15 @@ double run(int T=1){
 }
 int main(){
 	srand(time(0));
-	n1=9; n2=8;
+	//n1=9; n2=8;
 	//n1=8; n2=9;
 	//n1=11; n2=9;
 	//n1=9; n2=9;
-	//n1=n2=1000;
+	n1=n2=30000;
 	//n1=1000; n2=100;
 	vector<double> a;
 	for (int i1=1;;++i1){
-		int T=100000;
+		int T=1;
 		double ans=run(T);
 		a.push_back(ans);
 		printf("--------i1=%d #samples=%d %.6lf--------\n",i1,i1*T,mean(a));
